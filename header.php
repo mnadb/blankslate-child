5bb6fbf5e6b2a72012cb8129619b76411f3dedaa
Marie <mnad.contact@gmail.com>
2025-12-29 16:01:34 +0100

    commit initial

---
+++ header.php
@@ -0,0 +1,13 @@
+<header>
+    
+ <div class="logo">
+    <img src="<?php echo get_stylesheet_directory_uri();?>/images/Logo.png" alt="">
+ </div> 
+ <nav>
+    <a href="#">Accueil</a>
+     <a href="#">Nous rencontrer</a>
+     <button href="précommande">Commander</button>
+ </nav>
+ 
+
+</header>
