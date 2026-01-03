b6221f8902110cf97af021f709cb3c1bc144fa04
Marie <mnad.contact@gmail.com>
2026-01-03 17:48:20 +0100

    modification dans le header

--- header.php
+++ header.php
@@ -1,15 +1,42 @@
-<header>
-    
- <div class="logo">
-    <img src="<?php echo get_stylesheet_directory_uri();?>/images/Logo.png" alt="">
- </div> 
- 
- <nav>
-    <a href="#">Accueil</a>
-     <a href="#">Nous rencontrer</a>
-     <button> <a href="http://localhost:8080/planty/commande">Commander</a></button>
- </nav>
- 
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> <?php blankslate_schema_type(); ?>>
+
+<head>
+    <meta charset="<?php bloginfo('charset'); ?>">
+    <meta name="viewport" content="width=device-width">
+    <?php wp_head(); ?>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap" rel="stylesheet">
+</head>
+
+<body <?php body_class(); ?>>
+    <?php wp_body_open(); ?>
+    <header id="header" role="banner">
+        <div class="logo">
+            <?php
+            if (function_exists('the_custom_logo')) {
+                the_custom_logo();
+            }
+            ?>
+        </div>
+
+        <nav id="menu">
+            <div class="nav-menu">
+                <?php
+                wp_nav_menu([
+                    'theme_location' => 'header',
+                    'container' => false,
+                    'menu_class' => 'menu'
+                ])
+                ?>
+            </div>
 
 
-</header>
+            <button>
+                <a id="txt" href="http://localhost:8080/planty/commande/">commander</a>
+            </button>
+        </nav>
+    </header>
+
+    <main id="content" role="main">
--- page-commande.php
+++ page-commande.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Syne:wght@400..800&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css">
-    <title>Planty</title>
-    <?php wp_head(); ?>
-</head>
-
 <body>
     <?php wp_body_open(); ?>
     <?php get_header();  // inclure le fichier header.php 
@@ -19,11 +5,11 @@
 
 
     <main>
-    
-         
-          
-                <?php the_content(); ?>
-   
+
+
+
+        <?php the_content(); ?>
+
 
     </main>
     <?php get_footer();  // inclure le fichier footer.php 
@@ -31,4 +17,4 @@
     <?php wp_footer(); ?>
 </body>
 
-</html>   
+</html>
